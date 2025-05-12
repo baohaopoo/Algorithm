7b3323edf64b139fbbe00a506e7abefd04d72ba2
baohaopoo <53555682+baohaopoo@users.noreply.github.com>
2025-05-12 13:26:46 +0900

    [이민희] 자릿수 더하기 #Programmers-자릿수 더하기

---
+++ 자릿수더하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+	int answer = 0;
+
+	string str = to_string(n);
+	for (char c : str)
+	{
+		answer += c - '0';
+	}
+
+	return answer;
+}
