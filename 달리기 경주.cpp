6f5355258ac7f37c063539c6e742970c6fedb6ca
baohaopoo <53555682+baohaopoo@users.noreply.github.com>
2025-05-19 01:08:54 +0900

    [이민희] 달리기 경주 시간초과 버전 2 #programmers

--- 달리기 경주.cpp
+++ 달리기 경주.cpp
@@ -1,28 +1,22 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iostream>
 using namespace std;
-void SortPlayersByTarget(vector<string>& players, string& Target)
-{
-    for (int i = 0; i < players.size(); ++i)
-    {
-        if (players[i] == Target)
-        {
-            if (i > 0)
-            {
-                swap(players[i], players[i - 1]);
-            }
-        }
-    }
-}
+
 vector<string> solution(vector<string> players, vector<string> callings) {
 
-    //swap 정렬 
-    //Players를 정렬한다.
-    for (string ele : callings)
-    {
-        SortPlayersByTarget(players, ele);
-    }
+	//swap 정렬 
+	//Players를 정렬한다.
+	//calling에서 value값이 players 몇번째 문자열인지  찾아냄.
 
-    return players;
+	for (int i = 0; i < callings.size(); ++i)
+	{
+		auto iter = find(players.begin(), players.end(), callings[i]);
+		iter_swap(iter, iter - 1);
+	}
+
+	return players;
 }
+
+//버전 투 도 시간초과남.
