cd0c3e52eeba807e6695fd0da529dce862efdf5c
baohaopoo <53555682+baohaopoo@users.noreply.github.com>
2025-05-27 00:37:41 +0900

    [이민희] 예산 #programmers

--- 예산.cpp
+++ 예산.cpp
@@ -2,49 +2,20 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
 int solution(vector<int> d, int budget) {
 	int answer = 0;
-	int budgetTmp = budget;
 
-	vector<int> answers;
+	sort(d.begin(), d.end());
 
 	for (int i = 0; i < d.size(); ++i)
 	{
-		budgetTmp = budget;
-		budgetTmp -= d[i];
-		for (int j = i + 1; j < d.size(); ++j)
-		{
-			budgetTmp -= d[j];
-			for (int k = j + 1; k < d.size(); ++k)
-			{
-				budgetTmp -= d[k];
+		budget -= d[i];
 
-				if (budgetTmp >= 0)
-				{
-					answer++;
-					cout << d[i] << "," << d[j] << "," << d[k] << endl;
-				}
-
-				budgetTmp += d[k];
-			}
-			budgetTmp += d[j];
-		}
-		budgetTmp += d[i];
-
-	}
-
-	for (auto& iter : answers)
-	{
-		cout << iter << endl;
+		if (budget >= 0)
+			answer++;
 	}
 
 	return answer;
-}
-
-int main()
-{
-	vector<int> d = { 1,3,2,5,4 };
-	solution(d, 9);
 }
