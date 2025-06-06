d4f025fbc4d83ce46397d7e39c483581da17a9c5
baohaopoo <53555682+baohaopoo@users.noreply.github.com>
2025-06-06 23:53:22 +0900

    [이민희] 네트워크 dfs #programmers

--- 네트워크2.cpp
+++ 네트워크2.cpp
@@ -2,31 +2,31 @@
 #include <vector>
 
 using namespace std;
-
 void dfs(int node, const vector<vector<int>>& computers, vector<bool>& visited)
 {
-    for (int i = 0; i < computers[node].size(); ++i)
-    {
-        if (!visited[node] && !visited[i])
-        {
-            if (computers[node][i] == 1)
-                visited[i] = true;
-        }
-    }
+	for (int i = 0; i < computers[node].size(); ++i)
+	{
+		if (computers[node][i] == 1 && !visited[i])
+		{
+			visited[i] = true;
+			dfs(i, computers, visited);
+		}
+	}
 }
-
 int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
+	int answer = 0;
 
-    vector<bool> visited(n, false);
-    for (int i = 0; i < n; ++i)
-    {
-        if (!visited[i])
-        {
-            dfs(i, computers, visited);
-            answer++;
-        }
-    }
+	vector<bool> visited(n, false);
 
-    return answer;
+	for (int i = 0; i < n; ++i)
+	{
+		if (!visited[i])
+		{
+			dfs(i, computers, visited);
+			answer++;
+		}
+
+	}
+
+	return answer;
 }
